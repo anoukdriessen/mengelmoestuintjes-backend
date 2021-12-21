16f932d304a7accce77ef71151556c76372b3f6d
Anouk Driessen <info@anoukdriessen.nl>
2021-12-21 20:41:00 +0100

    update config files

---
+++ src/main/java/nl/mengelmoestuintjes/gardening/config/WebSecurityConfig.java
@@ -0,0 +1,82 @@
+package nl.mengelmoestuintjes.gardening.config;
+
+import nl.mengelmoestuintjes.gardening.model.users.security.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
+
+import static org.springframework.http.HttpMethod.PATCH;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private DataSource dataSource;
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    WebSecurityConfig( DataSource dataSource, JwtRequestFilter jwtRequestFilter ) {
+        this.dataSource = dataSource;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal( AuthenticationManagerBuilder auth ) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, authority FROM authorities AS a WHERE username=?");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected void configure( HttpSecurity http ) throws Exception {
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(PATCH,"/gebruikers/{^[\\w]$}/password").authenticated()
+                .antMatchers("/gebruikers/**").hasRole("ADMIN")
+                .antMatchers("/plants/**").hasRole("USER")
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
+
+/*
+                .antMatchers("/persons/**").hasAnyRole("USER")
+                .antMatchers(HttpMethod.GET, "hello").authenticated()
+                .antMatchers(HttpMethod.GET, "goodbye").permitAll()
+*/
