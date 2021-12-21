86346a6af247ff734660ac09ec480201a5a91920
Anouk Driessen <info@anoukdriessen.nl>
2021-12-21 19:41:40 +0100

    Update ExceptionController.java

--- src/main/java/nl/mengelmoestuintjes/gardening/controller/ExceptionController.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/controller/ExceptionController.java
@@ -1,6 +1,6 @@
 package nl.mengelmoestuintjes.gardening.controller;
 
-import nl.mengelmoestuintjes.gardening.exceptions.RecordNotFoundException;
+import nl.mengelmoestuintjes.gardening.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,5 +14,25 @@ public class ExceptionController {
     @ExceptionHandler(value = RecordNotFoundException.class)
     public ResponseEntity<Object> exception(RecordNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<Object> exception(BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = InvalidPasswordException.class)
+    public ResponseEntity<Object> exception(InvalidPasswordException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> exception(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = NotAuthorizedException.class)
+    public ResponseEntity<Object> exception(NotAuthorizedException exception) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
     }
 }
