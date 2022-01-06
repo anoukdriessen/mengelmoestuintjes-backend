41776224a53bde92c8893f19b215e067ac6869fe
Anouk Driessen <info@anoukdriessen.nl>
2022-01-06 13:10:33 +0100

    update security files
    zoekend naar het probleem met authorizatie

--- src/main/java/nl/mengelmoestuintjes/gardening/security/JwtRequestFilter.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/security/JwtRequestFilter.java
@@ -26,32 +26,35 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal( @NonNull HttpServletRequest request,
-                                     @NonNull HttpServletResponse response,
-                                     @NonNull FilterChain filterChain ) throws ServletException, IOException {
-        final String authorizationHeader = request.getHeader( "Authorization" );
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;
         String jwt = null;
 
-        if ( authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null ) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername( username );
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
-            if ( jwtUtil.validateToken( jwt, userDetails )) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities()
                 );
-
-                authToken.setDetails( new WebAuthenticationDetailsSource().buildDetails( request ) );
-                SecurityContextHolder.getContext().setAuthentication( authToken );
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
 
-        filterChain.doFilter( request, response );
+        filterChain.doFilter(request, response);
+
     }
+
 }
--- src/main/java/nl/mengelmoestuintjes/gardening/security/JwtUtil.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/security/JwtUtil.java
@@ -13,41 +13,50 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
-    private final static String SECRET_KEY = "supersecret";
 
-    public String extractUsername( String token ) {
-        return extractClaim( token, Claims::getSubject );
+    private final static String SECRET_KEY = "secret";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
-    private Date extractExpiration( String token ) {
-        return extractClaim( token, Claims::getExpiration );
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
-    private <T> T extractClaim( String token, Function<Claims, T> claimsResolver ) {
-        final Claims claims = extractAllClaims( token );
-        return claimsResolver.apply( claims );
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
-    private Claims extractAllClaims( String token ) {
-        return Jwts.parser().setSigningKey( SECRET_KEY ).parseClaimsJws( token ).getBody();
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
-    private Boolean isTokenExpired( String token ) {
-        return extractExpiration( token ).before( new Date() );
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
     }
-    public String generateToken( UserDetails userDetails ) {
+
+    public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken( claims, userDetails.getUsername() );
+        return createToken(claims, userDetails.getUsername());
     }
-    private String createToken( Map<String, Object> claims, String subject ) {
-        long validPeriod = 1000 * 60 * 60 * 24 * 10; // 10 days in ms
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        long validPeriod = 1000 * 60 * 60 * 24 * 10;   // 10 days in ms
         long currentTime = System.currentTimeMillis();
         return Jwts.builder()
-                .setClaims( claims )
-                .setSubject( subject )
-                .setIssuedAt( new Date( currentTime ) )
-                .setExpiration( new Date( currentTime + validPeriod ) )
-                .signWith( SignatureAlgorithm.HS256, SECRET_KEY )
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(currentTime + validPeriod))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
-    public Boolean validateToken( String token, UserDetails userDetails ) {
-        final String username = extractUsername( token );
-        return username.equals( userDetails.getUsername() ) && !isTokenExpired( token );
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
+
 }
