291a04a44474b9d48582919bdc3e029313da982e
Anouk Driessen <info@anoukdriessen.nl>
2022-01-06 13:33:21 +0100

    Update InvalidPasswordException.java

--- src/main/java/nl/mengelmoestuintjes/gardening/exceptions/InvalidPasswordException.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/exceptions/InvalidPasswordException.java
@@ -1,8 +1,8 @@
 package nl.mengelmoestuintjes.gardening.exceptions;
 
 public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException( String message ) {
-        super(message);
+    public InvalidPasswordException( String username ) {
+        super("Invalid password for user with username " + username);
     }
     public InvalidPasswordException() {
         super("Password is invalid");
