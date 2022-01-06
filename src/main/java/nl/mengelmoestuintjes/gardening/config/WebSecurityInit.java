590e85a684c68dd31afeea02d7e846f500afe200
Anouk Driessen <info@anoukdriessen.nl>
2022-01-06 13:08:11 +0100

    Delete WebSecurityInit.java
    volgens mij niet meer nodig

--- src/main/java/nl/mengelmoestuintjes/gardening/config/WebSecurityInit.java
+++
@@ -1,7 +0,0 @@
-package nl.mengelmoestuintjes.gardening.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class WebSecurityInit
-        extends AbstractSecurityWebApplicationInitializer {
-}
