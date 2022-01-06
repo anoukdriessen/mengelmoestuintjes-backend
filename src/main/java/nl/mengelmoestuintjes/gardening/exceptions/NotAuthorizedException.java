1aad2508861929ffe7a623dcc44e16b4e443d67e
Anouk Driessen <info@anoukdriessen.nl>
2022-01-06 13:09:30 +0100

    Update NotAuthorizedException.java

--- src/main/java/nl/mengelmoestuintjes/gardening/exceptions/NotAuthorizedException.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/exceptions/NotAuthorizedException.java
@@ -1,8 +1,8 @@
 package nl.mengelmoestuintjes.gardening.exceptions;
 
 public class NotAuthorizedException extends RuntimeException {
-    public NotAuthorizedException(String message) {
-        super(message);
+    public NotAuthorizedException(String username) {
+        super("User with credentials " + username + " is not authorized");
     }
     public NotAuthorizedException() {
         super("Not authorized");
