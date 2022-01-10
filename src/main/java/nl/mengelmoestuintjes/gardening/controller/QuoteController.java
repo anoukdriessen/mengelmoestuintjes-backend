895d4ddc2792ee1ec2ea688df09b33e080ffe5ca
Anouk Driessen <info@anoukdriessen.nl>
2022-01-10 14:25:21 +0100

    Update QuoteController.java

--- src/main/java/nl/mengelmoestuintjes/gardening/controller/QuoteController.java
+++ src/main/java/nl/mengelmoestuintjes/gardening/controller/QuoteController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/quotes")
+@RequestMapping(value = "/api/quotes")
 public class QuoteController {
     private final QuoteService quoteService;
 
